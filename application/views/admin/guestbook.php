d32654b1ff2dc591980a8c523e7f84536220d3b4
Nova <amazon3@foxmail.com>
2020-08-23 20:29:01 +0800

    Update guestbook.php
    Updated style

--- application/views/admin/guestbook.php
+++ application/views/admin/guestbook.php
@@ -6,8 +6,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <meta charset="UTF-8">
     <title>Nova's blog - 留言板</title>
-    <meta name="msvalidate.01" content="3ED833EEEC8B466015F352CA0CEC159F" />
-    <meta name="baidu-site-verification" content="Een7k8JYLQ" />
+    <meta name="msvalidate.01" content="" />
+    <meta name="baidu-site-verification" content="" />
     <meta name="keywords" content="Luna, Nova, Guestbook, Friends"/>
     <meta name="description" content="普通上班族的留言板，是什么Friends来我这里留言了呢ฅ( ̳• ◡ • ̳)ฅ"/>
     <link rel="stylesheet" href="<?php echo base_url();?>static/css/index.css"/>
