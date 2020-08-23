0007c8d975f7aa665cd7f800517c47e70be16d60
Nova <amazon3@foxmail.com>
2020-08-23 20:25:47 +0800

    Update search.php
    Updated style

--- application/views/search.php
+++ application/views/search.php
@@ -6,8 +6,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
     <meta charset="UTF-8">
     <title>Nova's Blog - 搜索结果</title>
-    <meta name="msvalidate.01" content="3ED833EEEC8B466015F352CA0CEC159F" />
-    <meta name="baidu-site-verification" content="Een7k8JYLQ" />
+    <meta name="msvalidate.01" content="" />
+    <meta name="baidu-site-verification" content="" />
     <meta name="keywords" content="Luna, Nova, Articles Search"/>
     <meta name="description" content="看看在普通上班族的博客里搜出了什么好东西？o(^・x・^)o"/>
     <link rel="stylesheet" href="<?php echo base_url();?>static/css/index.css"/>
